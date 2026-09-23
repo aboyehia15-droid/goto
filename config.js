4d2a2381205959b26368f32d10f44fa581381f80
aboyehia15-droid <aboyehia15@gmail.com>
2026-09-23 20:59:22 +0300

    Add files via upload

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // ضع هنا رابط تطبيق الويب الذي نسخته من Apps Script (يبدأ بـ https://script.google.com وينتهي بـ /exec)
 // مثال:  window.API_URL = 'https://script.google.com/macros/s/AKfycb.../exec';
-window.API_URL = 'https://script.google.com/macros/s/AKfycbwDWfNt9Kf6EnrICIC7WFZmAqPi8hn5k_mvy5AW6kf_eFruc55jgL6LgjbHcEQDGftSQA/exec';
+window.API_URL = 'ضع-الرابط-هنا';
